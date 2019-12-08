cf32145b0bd7c2d01509ad85b541b2edbfcc2637
lukasz.du2s <lukasz.du2s@gmail.com>
2019-12-08 19:11:21 +0100

    up: project cleanup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <empolyees/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Empolyees from "@/components/Empolyees.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Empolyees
   }
 };
 </script>
