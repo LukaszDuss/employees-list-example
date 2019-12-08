f0737c6a6b763fc41d3ba4ebd9391765ddcc9613
lukasz.du2s <lukasz.du2s@gmail.com>
2019-12-08 23:54:34 +0100

    up: patrialy done editing

--- src/components/Empolyees.vue
+++ src/components/Empolyees.vue
@@ -1,48 +1,84 @@
 <template>
   <div>
+    <div class="flex justify-between object-center bg-gray-500 m-2 py-4 rounded">
+      <span class="w-1/12">ID</span>
+      <span class="w-1/3">Employee Name</span>
+      <span class="w-1/6">Employee Salary</span>
+      <span class="w-1/6">Employee Age</span>
+      <div class="w-1/12"></div>
+    </div>
+    <div v-if="!list.lenght">
+      NO EMPLOYEES 
+    </div>
     <div
-      v-for="employee in list"
+      v-for="(employee, index) in (list.slice(0, limit))"
       v-bind:key="employee.id"
-      class="flex justify-between bg-gray-200 m-2 rounded"
+      class="flex justify-between object-center bg-gray-300 m-2 p-2 rounded"
     >
       <span class="w-1/12">{{employee.id}}</span>
-      <span class="w-1/3">{{employee.employee_name}}</span>
-      <span class="w-1/12">{{employee.employee_salary}}</span>
-      <span class="w-1/12">{{employee.employee_age}}</span>
-      <!-- <span class="w-1/12">{{employee.profile_image}}</span> -->
+      <span
+        v-if="!list[index].updating"
+        class="w-1/3"
+      >{{employee.employee_name}} {{list[index].updating}}</span>
+      <input
+        v-else
+        v-model="list[index].name"
+        class="w-1/3"
+        type="text"
+        placeholder="Please enter employee name..."
+      />
+      <span v-if="!list[index].updating" class="w-1/6">{{employee.employee_salary}}</span>
+      <input
+        v-else
+        v-model="list[index].salary"
+        class="w-1/6"
+        type="text"
+        placeholder="Please enter employee salary..."
+      />
+      <span v-if="!list[index].updating" class="w-1/6">{{employee.employee_age}}</span>
+      <input
+        v-else
+        v-model="list[index].age"
+        class="w-1/6"
+        type="text"
+        placeholder="Please enter employee age..."
+      />
       <div class="w-1/12">
-        <!-- <button> <Zondicon icon="add-outline" class="flex mx-2 w-4 fill-current"/></button> -->
-        <button @click="updateEmployee(employee)"> <Zondicon icon="cog" class="flex mx-2 w-4 fill-current"/></button>
-        <button @click="deleteEmployee(employee.id)" > <Zondicon icon="close-outline" class="flex mx-2 w-4 fill-current"/> </button>
+        <button v-if="!list[index].updating" @click="list[index].updating = true">
+          <Zondicon icon="cog" class="flex mx-2 w-4 fill-current" />
+        </button>
+        <button v-else @click="updateEmployee(index)">
+          <Zondicon icon="checkmark-outline" class="flex mx-2 w-4 fill-current" />
+        </button>
+        <button v-if="!list[index].updating" @click="deleteEmployee(employee.id)">
+          <Zondicon icon="close-outline" class="flex mx-2 w-4 fill-current" />
+        </button>
+        <button v-else @click="list[index].updating = false">
+          <Zondicon icon="block" class="flex mx-2 w-4 fill-current" />
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from "axios"; 
-import Zondicon from 'vue-zondicons'
+import axios from "axios";
+import Zondicon from "vue-zondicons";
 
 export default {
   name: "employees",
   data() {
     return {
+      limit: 20,
       list: [],
       errors: []
-    }
+    };
   },
   components: {
     Zondicon
   },
   created() {
-    axios
-      .get(`http://dummy.restapiexample.com/api/v1/employees`)
-      .then(response => {
-        this.list = response.data;
-      })
-      .catch(e => {
-        this.errors.push(e);
-      });
+    this.getEmployeesList();
   },
   methods: {
     async getEmployeesList() {
@@ -54,6 +90,7 @@ export default {
         .catch(e => {
           this.errors.push(e);
         });
+      this.update();
     },
     async getEmployee(id) {
       await axios
@@ -77,18 +114,22 @@ export default {
           this.errors.push(e);
         });
     },
-    async updateEmployee(updatedEmployee) {
+    async updateEmployee(index) {
+      this.list[index].updating = false;
+      let data = {
+        name: this.list[index].name,
+        salary: this.list[index].salary,
+        age: this.list[index].age
+      };
       await axios
         .put(
-          `http://dummy.restapiexample.com/api/v1/update/${updatedEmployee.id}`,
-          updatedEmployee
+          `http://dummy.restapiexample.com/api/v1/update/${this.list[index].id}`,
+          data
         )
         .then(response => {
-          this.list = this.list.filter(employee =>
-            employee.id == updatedEmployee.id
-              ? (employee = response.data)
-              : employee
-          );
+          this.list[index].employee_name = response.data.name;
+          this.list[index].employee_salary = response.data.salary;
+          this.list[index].employee_age = response.data.age;
         })
         .catch(e => {
           this.errors.push(e);
@@ -105,6 +146,9 @@ export default {
         .catch(e => {
           this.errors.push(e);
         });
+    },
+    async update() {
+      // this.$$forceUpdate();
     }
   }
 };
