45a69ac2df5aa3eb4e957de750ffd7bacab10234
lukasz.du2s <lukasz.du2s@gmail.com>
2019-12-15 17:11:39 +0100

    up:build

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,13 @@
 const autoprefixer = require('autoprefixer');
 const tailwindcss = require('tailwindcss');
+// const postcssPurgecss = require(`@fullhuman/postcss-purgecss`);
 
 module.exports = {
     plugins: [
       tailwindcss,
       autoprefixer,
-     ...process.env.NODE_ENV === 'production'
-       ? [purgecss]
-       : [],
+    //  ...process.env.NODE_ENV === 'production'
+    ///    ? [purgecss]
+    //    : [],
     ],
   }
